1170b2d6709746afeeaf9df95b2e93bcf630b6d8
garyballantyne <gary.ballantyne@haulashore.com>
2014-10-28 09:45:30 +1300

    Minor edits.

--- m/col_combo.m
+++ m/col_combo.m
@@ -4,10 +4,8 @@ function y = col_combo (A,k)
 % y is a cell array of matrices, formed by taking all possible combinations
 % of the columns of A, k at a time.
  
-[M,N]=size(A);
+[M,N] = size(A);
 combs = combnk(1:N,k); % all column combinations, taken k at a time
-AA=A(:,combs.'); % arrange col combinations horizontally
-y=mat2cell(AA,M,k*ones(1,size(combs,1))); % partition into cell array
-
-%!end (4)
+AA = A(:,combs.'); % arrange col combinations horizontally
+y = mat2cell(AA,M,k*ones(1,size(combs,1))); % partition into cell array
 
